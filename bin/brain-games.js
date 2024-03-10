25b1c2cdd0d4492cac084b09059a0e80fc880e36
Ivan Starikov <oldstarman@yandex.ru>
2024-03-10 22:51:24 +0300

    fix: bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { nameRequest } from '../src/cli';
+import { nameRequest } from '../src/cli.js';
 
 const welcomeMessage = 'Welcome to the Brain Games!';
 console.log(welcomeMessage);
