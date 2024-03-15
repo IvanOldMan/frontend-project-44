f088a6a42453f10510ef33ebc7127118f9ebc7d7
Ivan Starikov <oldstarman@yandex.ru>
2024-03-15 23:22:23 +0300

    fix: games/brain-even.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,34 +1,24 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import { nameRequest, userName } from '../src/cli.js';
-import { typeOfAnswer } from '../src/index.js';
+import { welcomeMessage, nameRequest } from '../src/cli.js';
+// eslint-disable-next-line import/named
+import {
+  typeOfAnswer, getRandomInt, expression, examination, finalMessage,
+} from '../src/index.js';
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
+welcomeMessage();
 nameRequest();
-
-let correctSession = true;
-
 console.log(typeOfAnswer.even);
+let isAnswersCorrect = true;
 
-for (let i = 0; i < 3; i++) {
-    const currentNumber = getRandomInt(0, 100);
-    const correctAnswer = currentNumber % 2 === 0 ? 'yes' : 'no';
-    console.log(`Question: ${currentNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ').toString();
-    if (userAnswer === correctAnswer) {
-        console.log('Correct!');
-    } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet\'s try again, ${userName}!`);
-        correctSession = false;
-        break;
-    }
+for (let i = 0; i < 3 && isAnswersCorrect; i++) {
+  const currentNumber = getRandomInt(0, 100);
+  const correctAnswer = currentNumber % 2 === 0 ? 'yes' : 'no';
+
+  expression(`${currentNumber}`);
+  isAnswersCorrect = examination(correctAnswer);
 }
-if (correctSession) {
-    console.log(`Congratulations, ${userName}!`);
+
+if (isAnswersCorrect) {
+  finalMessage();
 }
